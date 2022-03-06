73467ae93b4427240b78ef8a4d08b1ad0aa6e526
Martin Kluck <martinkluck05@gmail.com>
2022-03-06 19:46:38 -0300

    config: add .env configuration

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { createClient } from "@supabase/supabase-js";
+const { supabaseUrl, supabaseKey } = useRuntimeConfig();
+const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
+</script>
+
 <template>
   <div>
     <h1>Nuxt 3 Supabase TodoMVC</h1>
